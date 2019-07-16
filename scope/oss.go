c72df0bf9c62a9d2431c0d17d8bb7a1d275852a5
马志洋 <mazhiyang@shein.com>
2019-07-16 18:06:46 +0800

    temp

--- scope/oss.go
+++ scope/oss.go
@@ -16,20 +16,18 @@ func Upload(ctx iris.Context) bool {
 	// Get the file from the request.
 	file, header, err := ctx.FormFile("file")
 	if err != nil {
-		ctx.StatusCode(iris.StatusInternalServerError)
-		return response.Error(ctx, "Error while uploading: "+err.Error(), nil)
+		return response.Error(ctx, err.Error(), nil)
 	}
 	defer file.Close()
 	filename := header.Filename
 	fileSize := header.Size
 	out, err := os.OpenFile("./uploads/"+filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		ctx.StatusCode(iris.StatusInternalServerError)
-		return response.Error(ctx, "Error while uploading: "+err.Error(), nil)
+		return response.Error(ctx, err.Error(), nil)
 	}
 	defer out.Close()
 	io.Copy(out, file)
-	return response.Success(ctx, "Upload over"+string(fileSize), nil)
+	return response.Success(ctx, string(fileSize), nil)
 }
 
 /**
